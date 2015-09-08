d27e9b59d14b8fd581694a3d075508a68c626d43
Jansen Kantor <jkantor@ccs.neu.edu>
2015-09-08 14:27:19 -0400

    added lifealert

---
+++ Assets/Scenes/lifealert/assets/playerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	private float movex;
+	private float movey;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		movex = Input.GetAxis ("Horizontal");
+		movey = Input.GetAxis ("Vertical");
+		rb.velocity = new Vector2 (movex * speed, movey * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		checkifWin (other);
+	}
+
+	void OnTriggerStay(Collider other){
+		checkifWin (other);
+	}
+
+	void checkifWin(Collider other){
+		if (Input.GetButtonDown ("Button 1") || Input.GetButtonDown ("Button 2")) {
+			Debug.Log ("Win");
+			MiniGame.Instance.ReportWin ();
+		} else {
+			Debug.Log ("In zone but not winning");
+		}
+	}
+}
+
